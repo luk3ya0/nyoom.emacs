15f29e6c5bc9334f4cddb75f52bc5569cd53e499
luk3ya0 <oneTOinf@163.com>
2021-04-21 20:17:02 +0800

    add line-spacing in org-mode

--- config.el
+++ config.el
@@ -55,10 +55,14 @@
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+(setq display-line-numbers-type nil)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
+(add-hook 'org-mode-hook
+          (lambda ()
+            (setq-local line-spacing 0.3)))
+
 (setq org-directory "~/Documents/Org")
 
 
