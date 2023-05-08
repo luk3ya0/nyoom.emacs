0c996a183f02637810b3abee578bb67e85adb8a9
luk3ya0 <oneTOinf@163.com>
2023-05-08 12:55:01 +0800

    add attr-justify to align the image

--- modules/lang/org-ext/config.el
+++ modules/lang/org-ext/config.el
@@ -171,7 +171,6 @@
                                 ;; "rm -fr %b.out %b.log %b.tex %b.brf %b.bbl auto"
                                 ))
 
-
   (defun my/org-latex--get-tex-string ()
     "Return the content of the LaTeX fragment at point."
     (let ((datum (org-element-context)))
@@ -258,8 +257,7 @@
         (setq tex-string (string-replace ele "" tex-string)))
       (or
        (memq 40 (string-to-list tex-string))
-       (memq 123 (string-to-list tex-string)))
-      ))
+       (memq 123 (string-to-list tex-string)))))
 
   (defun my/latex-fragment-radical-p ()
     (string-match "sqrt" (my/org-latex--get-tex-string)))
@@ -346,10 +344,164 @@ JUSTIFICATION is a symbol for 'left, 'center or 'right."
     "After advice function to justify fragments."
     (scimax-org-latex-fragment-justify (or (plist-get org-format-latex-options :justify) 'center)))
 
+  (advice-add 'org--make-preview-overlay :after 'scimax-org-latex-fragment-justify-advice))
 
-  (advice-add 'org--make-preview-overlay :after 'scimax-org-latex-fragment-justify-advice)
-  (advice-add 'org-display-inline-images :after 'scimax-org-latex-fragment-justify-advice))
+;;; Org Image ────────────────────────────────────────────────────────────────────────
+(after! org
+  (defun scimax-org-display-inline-images (&optional include-linked refresh beg end)
+    "Display inline images.
 
+An inline image is a link which follows either of these
+conventions:
+
+  1. Its path is a file with an extension matching return value
+     from `image-file-name-regexp' and it has no contents.
+
+  2. Its description consists in a single link of the previous
+     type.  In this case, that link must be a well-formed plain
+     or angle link, i.e., it must have an explicit \"file\" type.
+
+Equip each image with the key-map `image-map'.
+
+When optional argument INCLUDE-LINKED is non-nil, also links with
+a text description part will be inlined.  This can be nice for
+a quick look at those images, but it does not reflect what
+exported files will look like.
+
+When optional argument REFRESH is non-nil, refresh existing
+images between BEG and END.  This will create new image displays
+only if necessary.
+
+BEG and END define the considered part.  They default to the
+buffer boundaries with possible narrowing."
+    (interactive "P")
+    (when (display-graphic-p)
+      (when refresh
+        (org-remove-inline-images beg end)
+        (when (fboundp 'clear-image-cache) (clear-image-cache)))
+      (let ((end (or end (point-max))))
+        (org-with-point-at (or beg (point-min))
+          (let* ((case-fold-search t)
+                 (file-extension-re (image-file-name-regexp))
+                 (link-abbrevs (mapcar #'car
+                                       (append org-link-abbrev-alist-local
+                                               org-link-abbrev-alist)))
+                 ;; Check absolute, relative file names and explicit
+                 ;; "file:" links.  Also check link abbreviations since
+                 ;; some might expand to "file" links.
+                 (file-types-re
+                  (format "\\[\\[\\(?:file%s:\\|attachment:\\|[./~]\\)\\|\\]\\[\\(<?file:\\)"
+                          (if (not link-abbrevs) ""
+                            (concat "\\|" (regexp-opt link-abbrevs))))))
+            (while (re-search-forward file-types-re end t)
+              (let* ((link (org-element-lineage
+                            (save-match-data (org-element-context))
+                            '(link) t))
+                     (linktype (org-element-property :type link))
+                     (inner-start (match-beginning 1))
+                     (path
+                      (cond
+                       ;; No link at point; no inline image.
+                       ((not link) nil)
+                       ;; File link without a description.  Also handle
+                       ;; INCLUDE-LINKED here since it should have
+                       ;; precedence over the next case.  I.e., if link
+                       ;; contains filenames in both the path and the
+                       ;; description, prioritize the path only when
+                       ;; INCLUDE-LINKED is non-nil.
+                       ((or (not (org-element-property :contents-begin link))
+                            include-linked)
+                        (and (or (equal "file" linktype)
+                                 (equal "attachment" linktype))
+                             (org-element-property :path link)))
+                       ;; Link with a description.  Check if description
+                       ;; is a filename.  Even if Org doesn't have syntax
+                       ;; for those -- clickable image -- constructs, fake
+                       ;; them, as in `org-export-insert-image-links'.
+                       ((not inner-start) nil)
+                       (t
+                        (org-with-point-at inner-start
+                          (and (looking-at
+                                (if (char-equal ?< (char-after inner-start))
+                                    org-link-angle-re
+                                  org-link-plain-re))
+                               ;; File name must fill the whole
+                               ;; description.
+                               (= (org-element-property :contents-end link)
+                                  (match-end 0))
+                               (match-string 2)))))))
+                (when (and path (string-match-p file-extension-re path))
+                  (let ((file (if (equal "attachment" linktype)
+                                  (progn
+                                    (require 'org-attach)
+                                    (ignore-errors (org-attach-expand path)))
+                                (expand-file-name path))))
+                    (when (and file (file-exists-p file))
+                      (let ((width (org-display-inline-image--width link))
+                            (old (get-char-property-and-overlay
+                                  (org-element-property :begin link)
+                                  'org-image-overlay)))
+                        (if (and (car-safe old) refresh)
+                            (image-flush (overlay-get (cdr old) 'display))
+                          (let ((image (org--create-inline-image file width)))
+                            (when image
+                              (let ((ov (make-overlay
+                                         (org-element-property :begin link)
+                                         (progn
+                                           (goto-char
+                                            (org-element-property :end link))
+                                           (skip-chars-backward " \t")
+                                           (point))))
+                                    (shift (- (org-element-property :begin link) (line-beginning-position)))
+                                    (justify (org-display-inline-image--justify link))
+                                    )
+                                ;; (message justify)
+                                ;; FIXME: See bug#59902.  We cannot rely
+                                ;; on Emacs to update image if the file
+                                ;; has changed.
+                                (image-flush image)
+                                (overlay-put ov 'display image)
+                                (overlay-put ov 'face 'default)
+                                (overlay-put ov 'org-image-overlay t)
+
+                                (setq space-left (- (window-max-chars-per-line) (/ (org-display-inline-image--width link) 9))
+                                      offset (floor (cond
+                                                     ((string= justify "center")
+                                                      (- (/ space-left 2) shift))
+                                                     (t
+                                                      0))))
+
+                                (when (>= offset 0)
+                                  (overlay-put ov 'before-string (make-string offset ?\ )))
+
+                                (overlay-put
+                                 ov 'modification-hooks
+                                 (list 'org-display-inline-remove-overlay))
+                                (when (boundp 'image-map)
+                                  (overlay-put ov 'keymap image-map))
+                                (push ov org-inline-image-overlays))))))))))))))))
+
+  (defun org-display-inline-image--justify (link)
+    (let ((justify "left"))
+        (let* ((case-fold-search t)
+               (par (org-element-lineage link '(paragraph)))
+               (attr-re "^[ \t]*#\\+attr_.*?: +.*?:justify +\\(\\S-+\\)")
+               (par-end (org-element-property :post-affiliated par))
+               ;; Try to find an attribute providing a :justify.
+               (attr-justify
+                (when (and par (org-with-point-at
+                                   (org-element-property :begin par)
+                                 (re-search-forward attr-re par-end t)))
+                  (match-string 1)))
+               (justify
+                (cond
+                 ;; Convert numeric justifys to numbers, converting percentages.
+                 ((string= attr-justify "center")
+                  "center")
+                 (t "left"))))
+          justify)))
+
+  (advice-add 'org-display-inline-images :override #'scimax-org-display-inline-images))
 
 ;;; TODO ────────────────────────────────────────────────────────────────────────
 (defface space-lock
